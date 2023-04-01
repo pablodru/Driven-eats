2db050033eeeab2daeb5890bfb4028fa7f8ff798
pablodru <pdrocha03@gmail.com>
2023-04-01 16:37:05 -0300

    versao sem whatsapp

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+let prato = '';
+
+
 function PratoSelecionado(botao) {
     const botaoAnterior = document.querySelector('.caixa-de-pratos .selecionado');
     if (botaoAnterior !== null) {;
@@ -5,6 +9,10 @@ function PratoSelecionado(botao) {
     }
 
     botao.classList.add('selecionado');
+    
+    VerificaSelecao();
+
+    prato = botao.querySelector('h4').innerHTML;
 }
 
 function BebidaSelecionada(botao) {
@@ -14,6 +22,8 @@ function BebidaSelecionada(botao) {
     }
 
     botao.classList.add('selecionado');
+    
+    VerificaSelecao();
 }
 
 function SobremesaSelecionada(botao) {
@@ -23,4 +33,30 @@ function SobremesaSelecionada(botao) {
     }
 
     botao.classList.add('selecionado');
+
+    VerificaSelecao();
+}
+
+function VerificaSelecao () {
+    const verificaPrato = document.querySelector('.caixa-de-pratos .selecionado')
+    const verificaBebida = document.querySelector('.caixa-de-bebidas .selecionado')
+    const verificaSobremesa = document.querySelector('.caixa-de-sobremesas .selecionado')
+    const verificador = document.querySelector('.footer >  div')
+
+    if (verificaPrato !== null){
+        if(verificaBebida !== null){
+            if(verificaSobremesa !== null){
+                verificador.classList.add('confirmado')
+                verificador.innerHTML='Fechar pedido'
+            }
+        }
+    }
+}
+
+const mensagem = 'Olá, gostaria de fazer o pedido:%0A' +
+    `Prato: ${prato}`
+
+
+function Whatsapp() {
+    window.location.href=`https://wa.me/+5599999999999?text=${mensagem}`;
 }
